b51e5b67376dac7823e1992ac82f8a7cda6c17ae
[Andres Perez] <mandresperez.uk@gmail.com>
2023-01-22 15:28:54 -0700

    Added mongoose cluster connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
